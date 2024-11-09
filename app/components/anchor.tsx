afb9a909c46f74477ba1a6b0a443856ce76d9a29
Mavia111 <86669415+Mavia111@users.noreply.github.com>
2024-11-09 16:35:27 +0500

    anchor.tsx

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export function Anc(props:string|number){
+export function Anc(props:string){
     return(
         <Link className="anc" href={props.link} target="_blank">Click Here</Link>
     );
