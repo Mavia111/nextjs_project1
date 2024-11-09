f45e8f9b2e96bd32d2204f1aabc704cb674156fb
Mavia111 <86669415+Mavia111@users.noreply.github.com>
2024-11-09 16:54:42 +0500

    headings.tsx

--- app/components/headings.tsx
+++ app/components/headings.tsx
@@ -1,16 +1,20 @@
-export const Heading1 = (props) =>{
+interface Ty {
+    name: string; // Use `string` instead of `String`
+}
+
+export const Heading1 = (props: Ty) =>{
     return(
         <h1 className="heading " >{props.name}</h1>
     );
 }
 
-export const Heading2 = (props) =>{
+export const Heading2 = (props: Ty) =>{
     return(
         <h2 className="heading2 " >{props.name}</h2>
     );
 }
 
-export const Heading3 = (props) =>{
+export const Heading3 = (props Ty) =>{
     return(
         <h2 className="heading3 " >{props.name}</h2>
     );
