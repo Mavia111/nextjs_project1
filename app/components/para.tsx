07222cf0c0ee9690164f7d74ee83063a4c66ee39
Mavia111 <86669415+Mavia111@users.noreply.github.com>
2024-11-09 16:56:05 +0500

    para.tsx

--- app/components/para.tsx
+++ app/components/para.tsx
@@ -1,11 +1,14 @@
-export const Para = (props) =>{
+interface Ty {
+    name: string; // Use `string` instead of `String`
+}
+export const Para = (props: Ty) =>{
 
     return(
         <p className="para">{props.name}</p>
     );
 }
 
-export const SubPara = (props) =>{
+export const SubPara = (props: Ty) =>{
 
     return(
         <p className="subpara">{props.name}</p>
