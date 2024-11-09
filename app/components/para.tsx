cf3c6a70d7e2d91aaeccf11733740771f1031f58
Mavia111 <86669415+Mavia111@users.noreply.github.com>
2024-11-09 16:27:51 +0500

    para.tsx

--- app/components/para.tsx
+++ app/components/para.tsx
@@ -1,11 +1,11 @@
-export const Para = (props:any) =>{
+export const Para = (props) =>{
 
     return(
         <p className="para">{props.name}</p>
     );
 }
 
-export const SubPara = (props:any) =>{
+export const SubPara = (props) =>{
 
     return(
         <p className="subpara">{props.name}</p>
