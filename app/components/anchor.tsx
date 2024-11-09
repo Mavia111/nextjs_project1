40b5429005629c7407c1fcbc46abca9da860329b
Mavia111 <86669415+Mavia111@users.noreply.github.com>
2024-11-09 16:41:08 +0500

    anchor.tsx

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
-
-export function Anc(props:string){
+const ty = new Set([
+  'BigInt',
+  // eslint-disable-next-line @typescript-eslint/internal/prefer-ast-types-enum
+  'Boolean',
+  'Number',
+  'Object',
+  // eslint-disable-next-line @typescript-eslint/internal/prefer-ast-types-enum
+  'String',
+  'Symbol',
+]);
+export function Anc(props:ty){
     return(
         <Link className="anc" href={props.link} target="_blank">Click Here</Link>
     );
